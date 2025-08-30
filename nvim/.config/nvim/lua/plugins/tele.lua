6c1350cc17dbc02d9cdb9d514492f316303d47d9
job-kiptoo-dev <kiptoojob73@gmail.com>
2025-08-30 17:02:31 +0300

    Reconfigured  my nvim config volume2

--- nvim/.config/nvim/lua/plugins/tele.lua
+++ nvim/.config/nvim/lua/plugins/tele.lua
@@ -1,13 +1,39 @@
 -- See `:help telescope` and `:help telescope.setup()`
-require("telescope").setup({
-	defaults = {
-		layout_strategy = "vertical",
-		layout_config = {
-			width = 0.95,
-			height = 0.95,
-			preview_height = 0.7,
-		},
-	},
-})
+-- require("telescope").setup({
+-- 	defaults = {
+-- 		layout_strategy = "vertical",
+-- 		layout_config = {
+-- 			width = 0.95,
+-- 			height = 0.95,
+-- 			preview_height = 0.7,
+-- 		},
+-- 	},
+-- })
 
 -- vim.keymap.set("n", "<Leader>sn", "<CMD>lua require('telescope').extensions.notify.notify()<CR>", silent
+
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		-- See `:help telescope` and `:help telescope.setup()`
+		require("telescope").setup({
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = {
+					width = 0.95,
+					height = 0.95,
+					preview_height = 0.7,
+				},
+			},
+		})
+
+		-- Keymaps
+		vim.keymap.set(
+			"n",
+			"<Leader>sn",
+			"<CMD>lua require('telescope').extensions.notify.notify()<CR>",
+			{ silent = true, desc = "Telescope notify" }
+		)
+	end,
+}
