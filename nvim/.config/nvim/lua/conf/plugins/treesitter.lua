6f836efad883b44d6bb9f3142a5e923c544db629
job-kiptoo-dev <kiptoojob73@gmail.com>
2026-01-23 01:31:49 +0300

    Tmux edit

--- nvim/.config/nvim/lua/conf/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/conf/plugins/treesitter.lua
@@ -1,39 +1,65 @@
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   build = ":TSUpdate",
+--
+--   config = function()
+--     require("nvim-treesitter.configs").setup({
+--         ensure_installed = {
+--           "c",
+--           "cpp",
+--           "python",
+--           "html",
+--           "css",
+--           "matlab",
+--           "javascript",
+--           "lua",
+--           "vim",
+--           "vimdoc",
+--           "query"
+--         },
+--
+--         sync_install = false,
+--         auto_install = true,
+--
+--         autopairs = {
+--           enable = true,
+--         },
+--
+--         highlight = {
+--           enable = true,
+--           additional_vim_regex_highlighting = false,
+--         },
+--
+--         indent = {
+--           enable = true,
+--           --disable = { "python", "c" } -- these and some other langs don't work well
+--         },
+--       })
+--   end
+-- }
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-
+  -- event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c",
-          "cpp",
-          "python",
-          "html",
-          "css",
-          "matlab",
-          "javascript",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query"
-        },
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
 
-        sync_install = false,
-        auto_install = true,
-
-        autopairs = {
-          enable = true,
-        },
-        
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-
-        indent = {
-          enable = true,
-          --disable = { "python", "c" } -- these and some other langs don't work well
-        },
-      })
-  end
+    configs.setup({
+      ensure_installed = {
+        "c", "cpp", "python", "html", "css",
+        "matlab", "javascript", "lua",
+        "vim", "vimdoc", "query",
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    })
+  end,
 }
