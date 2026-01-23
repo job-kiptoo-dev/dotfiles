e0b7e725ef30b1befc0e21f4c9b77fe0a3c3cd79
job-kiptoo-dev <kiptoojob73@gmail.com>
2026-01-23 16:51:27 +0300

    tmux  and  neovim

--- nvim/.config/nvim/lua/conf/plugins/colorScheme.lua
+++ nvim/.config/nvim/lua/conf/plugins/colorScheme.lua
@@ -1,11 +1,44 @@
+-- return {
+--   "projekt0n/github-nvim-theme",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("github-theme").setup({
+--       options = {
+--         styles = {
+--           comments = "italic",
+--           keywords = "bold",
+--         },
+--       },
+--     })
+--
+--     vim.cmd.colorscheme("github_dark")
+--   end,
+-- }
 return {
-  'rebelot/kanagawa.nvim',
-  lazy = false,
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
+  lazy = false,
   config = function()
-    require('kanagawa').setup {
-      theme = 'base', -- Set to 'base' to match tmux
-    }
-    vim.cmd.colorscheme 'kanagawa'
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte | frappe | macchiato | mocha
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        treesitter = true,
+        nvimtree = true,
+        telescope = true,
+        cmp = true,
+        gitsigns = true,
+        which_key = true,
+        native_lsp = {
+          enabled = true,
+        },
+      },
+    })
+
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
+
